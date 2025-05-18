fb41d05356f5880085cf6bd948155aac293f0f86
Marc Mansour <marcm9387@gmail.com>
2025-05-18 10:36:40 +0200

    Add Profile and DetailsPage routes to App component for enhanced navigation

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,8 +7,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import SearchPage from "./components/SearchPage/SearchPage";
+import Profile from "./components/Profile/Profile";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+
 function App() {
-  const navigate = useNavigate();
   return (
     <>
       <Navbar />
@@ -18,6 +20,8 @@ function App() {
         <Route path="/app/login" element={<LoginPage />} />
         <Route path="/app/register" element={<RegisterPage />} />
         <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/profile" element={<Profile />} />
+        <Route path="/app/details/:productId" element={<DetailsPage />} />
       </Routes>
     </>
   );
